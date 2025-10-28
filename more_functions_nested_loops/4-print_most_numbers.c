3f9aced506e974bc42aa991401788820bda9074a
Crypoune <a.messenet@gmail.com>
2025-10-28 10:36:38 +0100

    Task 4. I believe in numbers and signs

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints numbers 0 to 9 except 2 and 4, followed by a new line.
- */
+* print_most_numbers - Prints numbers 0 to 9
+*                      except 2 and 4, followed by a new line.
+*
+* Return: void
+*/
 void print_most_numbers(void)
 {
-    char numbers[] = "01356789";
-    int i;
+char numbers[] = "01356789";
+int i;
 
-    for (i = 0; numbers[i] != '\0'; i++)
-    {
-        _putchar(numbers[i]);
-    }
-    _putchar('\n');
+for (i = 0; numbers[i] != '\0'; i++)
+{
+_putchar(numbers[i]);
+}
+_putchar('\n');
 }
