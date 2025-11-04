e738a343c7c9cee34c1b2e826ca1916d5421e56e
Crypoune <a.messenet@gmail.com>
2025-11-04 10:07:05 +0100

    feat(memory): implémente les fonctions de manipulation de mémoire

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - remplit la mémoire avec une valeur constante
+ * @s: pointeur vers la zone mémoire à remplir
+ * @b: la valeur à copier
+ * @n: nombre d’octets à copier
+ *
+ * Return: pointeur vers la zone mémoire s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
