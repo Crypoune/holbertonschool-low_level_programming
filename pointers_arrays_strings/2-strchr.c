26dc94ccd93aadbc21f07e200b8285206ec7ce21
Crypoune <a.messenet@gmail.com>
2025-11-04 14:55:58 +0100

    style: fix Betty coding style compliance

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,15 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s)
-    {
-        if (*s == c)
-            return (s);
-        s++;
-    }
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
 
-    if (*s == c)
-        return (s);
+	if (*s == c)
+		return (s);
 
-    return (NULL);
+	return (NULL);
 }
