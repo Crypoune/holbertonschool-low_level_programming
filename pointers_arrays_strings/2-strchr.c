857ed4fadb6478e7f9e2c86f08008af588e19291
Crypoune <a.messenet@gmail.com>
2025-11-04 14:51:42 +0100

    feat(strings): implement _strchr to find char in string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - localise un caractère dans une chaîne
+ * @s: la chaîne à parcourir
+ * @c: le caractère à rechercher
+ *
+ * Return: pointeur vers la première occurrence de c dans s, ou NULL si absent
+ */
+char *_strchr(char *s, char c)
+{
+    while (*s)
+    {
+        if (*s == c)
+            return (s);
+        s++;
+    }
+
+    if (*s == c)
+        return (s);
+
+    return (NULL);
+}
