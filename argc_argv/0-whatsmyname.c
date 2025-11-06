8a3cf502b7f0d50c86584df923776936c39a47dd
Crypoune <a.messenet@gmail.com>
2025-11-06 17:05:09 +0100

    feat(argc_argv): print program name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - imprime le nom du programme
+ * @argc: nombre d’arguments
+ * @argv: tableau contenant les arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
