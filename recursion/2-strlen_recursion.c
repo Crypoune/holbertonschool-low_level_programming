f580035a893ec784221c8202335a66cef4aa0acf
Crypoune <a.messenet@gmail.com>
2025-11-06 12:42:03 +0100

    feat(recursion): implement _strlen_recursion function

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calcule la longueur d'une chaîne de caractères
+ * @s: la chaîne à mesurer
+ *
+ * Return: la longueur de la chaîne
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
