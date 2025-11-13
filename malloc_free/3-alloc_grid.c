f8d41c9ba7265d4b5a106e77f65c4620bb46ebb4
Crypoune <a.messenet@gmail.com>
2025-11-13 09:26:35 +0100

    feat: implement alloc_grid function to create a 2D array of integers

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,28 +11,25 @@
 int **alloc_grid(int width, int height)
 {
 	int **grid;
-	int h, w;
+	int h;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = malloc(sizeof(int *) * height);
+	grid = calloc(height, sizeof(int *));
 	if (grid == NULL)
 		return (NULL);
 
 	for (h = 0; h < height; h++)
 	{
-		grid[h] = malloc(sizeof(int) * width);
+		grid[h] = calloc(width, sizeof(int));
 		if (grid[h] == NULL)
 		{
-			for (w = 0; w < h; w++)
-				free(grid[w]);
+			while (h-- > 0)
+				free(grid[h]);
 			free(grid);
 			return (NULL);
 		}
-
-		for (w = 0; w < width; w++)
-			grid[h][w] = 0;
 	}
 
 	return (grid);
