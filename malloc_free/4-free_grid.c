007ff1047a8b3276fa159001ede8b648ffbf5243
Crypoune <a.messenet@gmail.com>
2025-11-13 14:29:14 +0100

    feat: implement free_grid to properly release 2D array memory

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,16 +3,16 @@
 
 /**
  * free_grid - frees a 2D grid created by alloc_grid
- * @grid: the 2D grid to free
+ * @grid: pointer to the 2D array
  * @height: number of rows in the grid
  *
- * Return: Nothing.
+ * Return: void
  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL)
+	if (grid == NULL || height <= 0)
 		return;
 
 	for (i = 0; i < height; i++)
