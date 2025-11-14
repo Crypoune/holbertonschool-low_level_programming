f11f0eedf201cb29f21fab290e18b1d8867f4fd4
Crypoune <a.messenet@gmail.com>
2025-11-14 11:40:26 +0100

    feat: implement _calloc function with zeroed memory allocation

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array using malloc and sets it to zero
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: pointer to allocated zeroed memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i, total;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total = nmemb * size;
+	p = malloc(total);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < total; i++)
+		p[i] = 0;
+
+	return (p);
+}
