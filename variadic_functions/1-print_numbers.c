09283a3d4b3b7148f43bfa9527445717479a9315
Crypoune <a.messenet@gmail.com>
2025-11-19 21:44:41 +0100

    feat: implement print_numbers function

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print numbers separated by a separator
+ * @n: number of arguments
+ * @separator: separator displayed between numbers
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(args);
+
+	printf("\n");
+}
