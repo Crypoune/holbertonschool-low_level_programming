8107ec8c7fdc847785d73dec313f32f00a7cf5d7
Crypoune <a.messenet@gmail.com>
2025-11-19 21:49:28 +0100

    feat: implement print_numbers function

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,17 +17,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (n == 0)
 		return;
 
-	if (separator == NULL)
-	{
-		separator = "";
-	}
-
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 	va_end(args);
